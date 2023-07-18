b43697cfd0fbd4361e2e5a4a20dfe0bd397258eb
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-07-18 23:17:30 +0600

    a variable declared & print

--- main.cpp
+++ main.cpp
@@ -2,10 +2,28 @@
 using namespace std;
 
 int main()
-{
-    cout << "hello git" << endl;
+{ 
+    int a = 10;
+    cout << "hello git" << a <<  endl;
     return 0;
 }
 
-// git init --> empty repo initialise korbe
+// git add main.cpp
+
+
+
+
+
+// git init 
 // git add . 
+// git commit -m "initial coding start"
+
+// git branch -M main
+
+//git remote add origin https://github.com/Saif-Tasnim/Practise-Sample.git
+
+// git push -u origin main
+
+
+
+//git rm -r main.cpp
